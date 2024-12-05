f071d795ac768e7c4504b00b2e6f74372ce0c07a
Kylie Brunworth <kyliebrunworth@Kylies-Laptop.attlocal.net>
2024-12-04 23:11:26 -0600

    updating

---
+++ index.js
@@ -0,0 +1,17 @@
+/*#TDD
+
+1. A function called "multiplication" that returns the product of the two input numbers.
+ (1) Expect function to account for negative & decimal numbers.
+ (2) Expect function to return an error when user inputs a string.
+ (3) Expect function to return a correct answer.
+
+2. A function called "concatOdds" takes two arrays of integers as arguments. It should return a single array that only contains the odd numbers, in ascending order, from both of the arrays.
+ (1) Expect function to return only whole positive or negative odd numbers.
+ (2) Expect function to return an error if a string or decimal is entered.
+ (3) Expect function to return only one of a specific number if there are multiple of the same number in an array.
+
+3. A shopping cart checkout feature that allows a user to check out as a guest (without an account), or as a logged-in user. They should be allowed to do either, but should be asked if they want to create an account or log in if they check out as a guest.
+ (1) The user should be alerted to keep shopping if they try to check out with an empty cart.
+ (2) There should be an option to change the password.
+ (3) There should be an option to contact customer support in the form of a chatbot assistant.
+*/
